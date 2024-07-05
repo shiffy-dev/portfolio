55501d904251ddc0ccf2ccedb67566a5621bbaf0
shifa <shifa.yashfeen@gmail.com>
2024-07-05 15:00:20 +0400

    optimised ver

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 
-import About from './components/About';
-import Home from "./components/Home";
-import Navbar from './components/Navbar/Navbar';
-import Skills from './components/Skills';
+ import About from './components/About';
+ import Home from "./components/Home";
+//  import Navbar from './components/Navbar/Navbar';
+ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+
+
 
 function App() {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar  /> */}
       <Home />
       <About />
       <Skills />
       <div id="projects">
-        <Projects />
+      <Projects />
       </div>
       <Contact />
       
